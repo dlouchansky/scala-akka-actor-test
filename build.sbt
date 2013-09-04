58f180362371c5c72e35a4e3b10a225e2132a6b7
Daniel <bekasik@gmail.com>
2013-09-05 00:02:50 +0300

    Base methods implemented

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "Akka actor test"
+
+version := "1.0"
+
+scalaVersion := "2.10.2"
+ 
+resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+ 
+libraryDependencies +=
+  "com.typesafe.akka" %% "akka-actor" % "2.2.1"
--- src/main/scala/com/test/Main.scala
+++ src/main/scala/com/test/Main.scala
@@ -4,49 +4,22 @@ import akka.actor.{Kill, ActorRef, Actor, Props}
 
 object Main {
 
-  case object Done;
+  case object Done
 }
 
 class Main extends Actor {
 
+  var stopped: Int = 0
+
   override def preStart() {
-    killActors(createActors(5))
+    Worker.killActors(Worker.createActors(5, context))
   }
 
   def receive = {
-    case Main.Done ⇒ context.stop(self)
-  }
-
-  def createActors(n: Int): List[ActorRef] = {
-    createActors(n, List())
-  }
-
-  def createActors(n: Int, store: List[ActorRef]): List[ActorRef] = {
-    if (n == 0)
-      List()
-    else {
-      val greeter = context.actorOf(Props[Worker])
-
-      if (n == 1)
-        greeter :: store
-      else {
-        createActors(n - 1, greeter :: store)
-      }
+    case Main.Done ⇒ {
+      stopped = stopped + 1
     }
   }
 
-  def killActors(store: List[ActorRef]) {
-    store.foreach(actor => actor ! Kill)
-  }
-
-  def sendMessagesToOne(count: Int, actor: ActorRef) {
-    if (count != 0) {
-      actor ! Worker.Add
-      sendMessagesToOne(count - 1, actor)
-    }
-  }
-
-  def sendMessages(actorIds: List(Int), actors: List(ActorRef)) {
-  }
 }
 
